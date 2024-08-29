c94e4581a83dea7a22ebf08c57603467883a4eca
mike123ike <mike123.ike@gmail.com>
2024-08-28 20:03:01 -0500

    make venv work

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 def install(package):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', package])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
 install('pygame')
 
